7dcc1f42c312f63c983cb56be009320a27fbd020
Parabola949 <Parabola949@58556696-9666-11de-9ca7-6d167affc1ef>
2009-08-31 22:12:21 +0000

    User settings are now saved: window location, input settings

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -34,5 +34,53 @@ namespace Weather.Properties {
                 this["defaultCity"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("10, 20")]
+        public global::System.Drawing.Point windowPosition {
+            get {
+                return ((global::System.Drawing.Point)(this["windowPosition"]));
+            }
+            set {
+                this["windowPosition"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Never")]
+        public string intervalText {
+            get {
+                return ((string)(this["intervalText"]));
+            }
+            set {
+                this["intervalText"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool timerOn {
+            get {
+                return ((bool)(this["timerOn"]));
+            }
+            set {
+                this["timerOn"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("60000")]
+        public int intervalTime {
+            get {
+                return ((int)(this["intervalTime"]));
+            }
+            set {
+                this["intervalTime"] = value;
+            }
+        }
     }
 }
